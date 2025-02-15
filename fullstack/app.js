a592c1aca23c43e862285f348e51fd1002c7793e
aakashdeveloper <ahanda205@gmail.com>
2025-02-15 09:27:00 +0530

    mongodb

--- fullstack/app.js
+++ fullstack/app.js
@@ -1,12 +1,28 @@
 let express = require('express');
 let app = express();
 let port = 9801;
-let categoryRouter = require('./src/controller/categoryRouter')();
-let productRouter = require('./src/controller/productRouter')();
+let {dbConnect} = require('./src/controller/dbcontroller');
+
+let menu = [
+    {link:'/',name:'Home'},
+    {link:'/category',name:'Category'},
+    {link:'/products',name:'Products'}
+]
+
+let categoryRouter = require('./src/controller/categoryRouter')(menu);
+let productRouter = require('./src/controller/productRouter')(menu);
+
+//static file path
+app.use(express.static(__dirname+'/public'))
+//ejs file path
+app.set('views','./src/views')
+// view engine
+app.set('view engine','ejs')
 
 //default route
 app.get('/',(req,res) => {
-    res.send("Hii From Express")
+    //res.send("Hii From Express")
+    res.render('index',{title:'Home Page'})
 });
 
 
@@ -14,6 +30,7 @@ app.use('/category',categoryRouter)
 app.use('/products',productRouter)
 
 app.listen(port,(err) => {
+    dbConnect()
     if(err) throw err;
    // console.log("Server is running on port "+port)
     console.log(`Server is running on port ${port}`)
